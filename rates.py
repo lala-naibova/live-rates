7836d9ae04d8fd7cf9a09f2b0cb104ab2c6fe7c1
Lala Naibova <lala.naibova@gmail.com>
2019-07-12 17:23:38 +0400

    Support AZN in get_rate

--- rates.py
+++ rates.py
@@ -3,6 +3,10 @@ import re
 
 
 def get_rate(currency):
+
+    if currency.lower() == 'azn':
+        return 1
+
     request = requests.get('http://currency.az')
     content = request.content.decode()
     pattern = f'[a-z <">=]+1 {currency}[a-z /"<>=-]+(?P<rate>[0-9.]+) AZN'
@@ -15,9 +19,16 @@ yes_no = 'y'
 while yes_no.lower() == 'y':
     cur1 = input('From:')
     cur2 = input('To:')
+    if cur1.lower() == cur2.lower():
+        print('Currencies are same, please try again')
+        continue
+
     amount = float(input('How much? '))
+
     m_from = amount * get_rate(cur1)
+
     rate = get_rate(cur2)
+
     print(round(m_from / rate, 2), cur2.upper())
     yes_no = input('Do you want to continue? (y/n): ')
 
